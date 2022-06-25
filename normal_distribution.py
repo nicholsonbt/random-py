693f335fd1849bfbb4256b73fffca109c16a6a65
Ben Nicholson <bennicholson777@gmail.com>
2022-06-25 13:27:16 +0100

    Automate the generation of reasonable SD and skew values given minimum, maximum and mean values

--- normal_distribution.py
+++ normal_distribution.py
@@ -44,9 +44,28 @@ def RandomNormalSkewedBound(mean, sd, minimum, maximum, skew=0):
     # Return the number if within the bounds.
     return r
 
-def Experiment(minimum, maximum, mean, sd, skew):
+def RandomNormalSkewedSimplified(mean, minimum, maximum):
+    # Calculate the median value between minimum and maximum.
+    median = (maximum + minimum) / 2
+
+    # Calculate the offset of the mean from the median.
+    offset = median - mean
+
+    # Calculate the SD by dividing the range by 2pi.
+    # From my experiments, this gives a very good SD that gives samples covering the entire range
+    # but results in only ~0.1 being outside the bounds.
+    sd = (maximum - minimum) / (2 * math.pi)
+
+    # I derived this from the Fisher-Pearson coefficient of skewness.
+    skew = math.pow(offset, 3) / math.pow(sd,3)
+
+    # Generate and return a random number using the above calculations to estimate reasonable SD
+    # and skew values.
+    return RandomNormalSkewedBound(mean, sd, minimum, maximum, skew)
+
+def Experiment(minimum, maximum, mean):
     # Get 10000 data samples.
-    data = [RandomNormalSkewedBound(mean, sd, minimum, maximum, skew) for i in range(10000)]
+    data = [RandomNormalSkewedSimplified(mean, minimum, maximum) for i in range(10000)]
 
     # Plot the samples on the histogram with 100 bins.
     plt.hist(data, 100)
@@ -56,11 +75,7 @@ def Experiment(minimum, maximum, mean, sd, skew):
 
 # Only run the experiment if this is the main script.
 if __name__ == "__main__":
-    Experiment(0, 20, 15, 0.5, 3)
-
-
-
-
+    Experiment(5, 15, 13)
 
 
 
